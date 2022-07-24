0d35c84c3773ce149146fbb2daf94decf7848960
Cornelius Venti <corneliusventi@gmail.com>
2022-07-24 19:59:25 +0700

    add open graph and twitter meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,6 +38,19 @@ const Home: NextPage<Props> = ({ command }) => {
             href: "https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&display=swap",
           },
         ]}
+        openGraph={{
+          type: "website",
+          url: "https://corneliusventi.dev",
+          title: "Cornelius Venti",
+          description: command.description,
+          images: [{ url: "https://corneliusventi.dev/cover.png" }],
+          site_name: "Cornelius Venti",
+        }}
+        twitter={{
+          handle: "@corneliusventi",
+          site: "@corneliusventi",
+          cardType: "summary_large_image",
+        }}
       />
       <Terminal />
     </>
